32bb098b08225eee070f128b270b94349f674371
OZakhlivny <oleg.zakhlivny@gmail.com>
2020-05-10 16:20:22 +0300

    Homework 3 - подготовка к выполнению ДЗ

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,38 +1,36 @@
 package ru.geekbrains.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture img, backgroundImage;
+    private Background background;
 
-    private Vector2 position;
-    private Vector2 velocity;
-    private Vector2 touchPosition;
 
     @Override
     public void show() {
         super.show();
         backgroundImage = new Texture("space.jpg");
         img = new Texture("ufo.png");
-        position = new Vector2();
-        velocity = new Vector2();
-        touchPosition = new Vector2();
+        background = new Background(backgroundImage);
+
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        //if(position.epsilonEquals(touchPosition, 1)) velocity.set(0, 0);
-        if(touchPosition.cpy().sub(position).len() < velocity.len()) velocity.set(0, 0);
-        position.add(velocity);
         batch.begin();
-        batch.draw(backgroundImage, 0, 0);
-        batch.draw(img, position.x, position.y);
+        background.draw(batch);
         batch.end();
     }
 
@@ -41,15 +39,5 @@ public class MenuScreen extends BaseScreen {
         img.dispose();
         backgroundImage.dispose();
         super.dispose();
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touchPosition.set(screenX, Gdx.graphics.getHeight() - screenY);
-        System.out.printf("touchDown touchPosition.x = %.0f, touchPosition.y = %.0f\n", touchPosition.x, touchPosition.y);
-        velocity.set(touchPosition);
-        velocity.sub(position);
-        velocity.nor();
-        return super.touchDown(screenX, screenY, pointer, button);
     }
 }
