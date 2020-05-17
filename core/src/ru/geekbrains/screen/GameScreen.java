9c73ddb9608381b3fd2bfe54c0e7de879b9cc493
OZakhlivny <oleg.zakhlivny@gmail.com>
2020-05-17 13:16:57 +0300

    Homework 5 - подготовка к выполнению ДЗ

--- core/src/ru/geekbrains/screen/GameScreen.java
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
 import ru.geekbrains.sprite.Background;
 import ru.geekbrains.sprite.Star;
 import ru.geekbrains.sprite.Starship;
@@ -18,6 +19,8 @@ public class GameScreen extends BaseScreen {
     private TextureAtlas atlas;
     private Starship starship;
     private Star[] stars;
+    private BulletPool bulletPool;
+
 
 
     @Override
@@ -26,8 +29,9 @@ public class GameScreen extends BaseScreen {
         backgroundImage = new Texture("textures/bg.png");
         background = new Background(backgroundImage);
         atlas = new TextureAtlas(Gdx.files.internal("textures/mainAtlas.tpack"));
-        starship = new Starship(atlas);
-        stars = new Star[256];
+        bulletPool = new BulletPool();
+        starship = new Starship(atlas, bulletPool);
+        stars = new Star[64];
         for (int i = 0; i < stars.length; i++) stars[i] = new Star(atlas);
     }
 
@@ -35,20 +39,26 @@ public class GameScreen extends BaseScreen {
     public void render(float delta) {
         super.render(delta);
         update(delta);
+        free();
         draw();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
-        starship.resize(worldBounds);
         for (Star star : stars) star.resize(worldBounds);
+        starship.resize(worldBounds);
+    }
+
+    private void free() {
+        bulletPool.freeAllDestroyed();
     }
 
     @Override
     public void dispose() {
         backgroundImage.dispose();
         atlas.dispose();
+        bulletPool.dispose();
         super.dispose();
     }
 
@@ -60,23 +70,26 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
-        return super.touchUp(touch, pointer, button);
+        starship.touchUp(touch, pointer, button);
+        return false;
     }
 
     @Override
     public boolean keyDown(int keycode) {
         starship.keyDown(keycode);
-        return super.keyDown(keycode);
+        return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
         starship.keyUp(keycode);
-        return super.keyUp(keycode);
+        return false;
     }
 
     private void update(float delta) {
         for (Star star : stars) star.update(delta);
+        bulletPool.updateActiveSprites(delta);
+        starship.update(delta);
     }
 
     private void draw(){
@@ -84,6 +97,7 @@ public class GameScreen extends BaseScreen {
         background.draw(batch);
         for (Star star : stars) star.draw(batch);
         starship.draw(batch);
+        bulletPool.drawActiveSprites(batch);
         batch.end();
     }
 }
--- core/src/ru/geekbrains/sprite/Starship.java
+++ core/src/ru/geekbrains/sprite/Starship.java
@@ -1,69 +1,145 @@
 package ru.geekbrains.sprite;
 
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.Sprite;
 import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
 
 public class Starship extends Sprite {
-    private Vector2 velocity;
-    private Vector2 touchPosition;
-    private Vector2 position;
+
+    private static final float SIZE = 0.15f;
+    private static final float MARGIN = 0.05f;
+    private static final int INVALID_POINTER = -1;
+
     private Rect worldBounds;
 
+    private final Vector2 velocityZero;
+    private final Vector2 velocity;
 
-    public Starship(TextureAtlas atlas) {
+    private int leftPointer;
+    private int rightPointer;
+
+    private boolean pressedLeft;
+    private boolean pressedRight;
+
+    private BulletPool bulletPool;
+    private TextureRegion bulletRegion;
+    private Vector2 bulletV;
+
+
+    public Starship(TextureAtlas atlas, BulletPool bulletPool) {
         super(atlas.findRegion("main_ship"), 2, 1);
+        this.bulletPool = bulletPool;
         velocity = new Vector2();
-        touchPosition = new Vector2();
-        position = new Vector2();
-        worldBounds = new Rect();
+        velocityZero = new Vector2(0.5f, 0);
+        bulletV = new Vector2(0, 0.5f);
+        bulletRegion = atlas.findRegion("bulletMainShip");
+        leftPointer = rightPointer = INVALID_POINTER;
     }
 
     @Override
     public void resize(Rect worldBounds) {
         this.worldBounds = worldBounds;
-        setHeightProportion(0.3f);
-        this.pos.set(worldBounds.pos);
+        setHeightProportion(SIZE);
+        setBottom(worldBounds.getBottom() + MARGIN);
     }
 
     @Override
-    public void draw(SpriteBatch batch) {
-        if(position.set(touchPosition).sub(pos).len() < velocity.len()) velocity.set(0, 0);
-        pos.add(velocity);
-        super.draw(batch);
+    public void update(float delta) {
+        pos.mulAdd(velocity, delta);
+        if (getLeft() < worldBounds.getLeft()) {
+            stop();
+            setLeft(worldBounds.getLeft());
+        }
+        if (getRight() > worldBounds.getRight()) {
+            stop();
+            setRight(worldBounds.getRight());
+        }
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        touchPosition.set(touch);
-        velocity.set(touchPosition).sub(pos).setLength(0.005f);
-        return super.touchDown(touch, pointer, button);
+        if (touch.x < worldBounds.pos.x) {
+            if (leftPointer != INVALID_POINTER) return false;
+            leftPointer = pointer;
+            moveLeft();
+        } else {
+            if (rightPointer != INVALID_POINTER) return false;
+            rightPointer = pointer;
+            moveRight();
+        }
+        return false;
     }
 
-    public void keyDown(int keycode) {
-        if(keycode != Keys.UP && keycode != Keys.DOWN && keycode != Keys.LEFT && keycode != Keys.RIGHT) return;
-        switch(keycode) {
-            case Keys.UP:
-                touchPosition.set(pos.x, worldBounds.getTop() - getHalfHeight());
-                break;
-            case Keys.DOWN:
-                touchPosition.set(pos.x, worldBounds.getBottom() + getHalfHeight());
-                break;
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        if (pointer == leftPointer) {
+            leftPointer = INVALID_POINTER;
+            if (rightPointer != INVALID_POINTER) moveRight();
+            else stop();
+        } else if (pointer == rightPointer) {
+            rightPointer = INVALID_POINTER;
+            if (leftPointer != INVALID_POINTER) moveLeft();
+            else stop();
+        }
+        return false;
+    }
+
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Keys.A:
             case Keys.LEFT:
-                touchPosition.set(worldBounds.getLeft() + getHalfWidth(), pos.y);
+                pressedLeft = true;
+                moveLeft();
                 break;
+            case Keys.D:
             case Keys.RIGHT:
-                touchPosition.set(worldBounds.getRight() - getHalfWidth(), pos.y);
+                pressedRight = true;
+                moveRight();
+                break;
+            case Keys.UP:
+                shoot();
                 break;
         }
-        velocity.set(touchPosition).sub(pos).setLength(0.005f);
+        return false;
     }
 
-    public void keyUp(int keycode) {
-        if(keycode == Keys.UP || keycode == Keys.DOWN || keycode == Keys.LEFT || keycode == Keys.RIGHT) velocity.set(0, 0);
+    public boolean keyUp(int keycode) {
+        switch (keycode) {
+            case Keys.A:
+            case Keys.LEFT:
+                pressedLeft = false;
+                if (pressedRight) moveRight();
+                else stop();
+                break;
+            case Keys.D:
+            case Keys.RIGHT:
+                pressedRight = false;
+                if (pressedLeft) moveLeft();
+                else stop();
+                break;
+        }
+        return false;
+    }
+
+    private void moveRight() {
+        velocity.set(velocityZero);
+    }
+
+    private void moveLeft() {
+        velocity.set(velocityZero).rotate(180);
+    }
+
+    private void stop() {
+        velocity.setZero();
+    }
+
+    private void shoot() {
+        Bullet bullet = bulletPool.obtain();
+        bullet.set(this, bulletRegion, pos, bulletV, 0.01f, worldBounds, 1);
     }
 }
